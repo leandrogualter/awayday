8352fd0336a26fb4da204ef8caa4bddf2fdb841a
Rafael Lisboa <rafaelzlisboa@gmail.com>
2012-09-15 18:48:48 -0300

    set correct new deadline

--- awayday.rb
+++ awayday.rb
@@ -15,7 +15,7 @@ class AwayDayApp < Sinatra::Base
   enable :sessions
   use Rack::Flash, :sweep => true
 
-  DEADLINE = Time.new(2012, 9, 29, 9, 0, 0, "-03:00")
+  DEADLINE = Time.new(2012, 9, 26, 9, 0, 0, "-03:00")
 
   assets do
     serve '/css', from: '/assets/css'
