98d00b1160e2e38fb6381b7d854f8318ab5ec466
Leandro Gualter <leandrogualter@gmail.com>
2014-08-06 22:59:12 -0300

    Adds snap cache to bundle

--- .init.sh
+++ .init.sh
@@ -1,3 +1,20 @@
+#!/bin/bash
+
+set -e
+
+export BUNDLE_JOBS="${BUNDLE_JOBS:=4}"
+if [ -z "$SNAP_CI" ]; then
+  BUNDLE_PATH=/tmp/.bundle-$(basename $(pwd))
+else
+  BUNDLE_PATH=$HOME/.bundle;
+fi
+
+mkdir -p $BUNDLE_PATH
+rm -rf vendor/bundle
+ln -sf $BUNDLE_PATH vendor/bundle
+
+export NOKOGIRI_USE_SYSTEM_LIBRARIES=1
+
 bundle check 1>/dev/null
 
 if [ $? -ne 0 ]; then
